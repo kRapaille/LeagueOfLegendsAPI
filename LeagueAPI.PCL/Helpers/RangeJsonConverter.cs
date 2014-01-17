4e7b45f7ab60b92c6f90719afa0905972cc25ed8
Kévin Rapaille <kevin.rapaille@hotmail.com>
2014-01-17 15:19:37 +0100

    RangeJsonConverter was untracked

---
+++ LeagueAPI.PCL/Helpers/RangeJsonConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PortableLeagueAPI.Helpers
+{
+    internal class RangeJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var result = 0;
+
+            if (reader.ValueType == typeof(string))
+            {
+                result = 0;
+            }
+            else if (reader.ValueType == null)
+            {
+                reader.Read();
+                result = (int)((Int64)reader.Value);
+                reader.Read();
+            }
+
+            return result;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
