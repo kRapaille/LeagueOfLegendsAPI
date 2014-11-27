42389d71c8308ef94933d81b7f3efd9455e6c9ab
Bcpoole <brandoncpoole@gmail.com>
2014-11-26 23:14:56 -0600

    added effect

--- PortableLeagueApi.Static/Models/BasicData.cs
+++ PortableLeagueApi.Static/Models/BasicData.cs
@@ -18,8 +18,6 @@ namespace PortableLeagueApi.Static.Models
 
         public string Description { get; set; }
 
-        public Dictionary<string, string> Effect { get; set; }
-
         public IList<string> From { get; set; }
 
         public IGold Gold { get; set; }
